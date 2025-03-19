d06e2d8f2255accc35aaf21e648f60f8201f543e
Arina <arina.safronova@inbox.ru>
2025-03-19 22:05:42 +0700

    Отформатирован код

--- cooking/web_servs/service.js
+++ cooking/web_servs/service.js
@@ -1,6 +1,8 @@
+/****************************************************
+ *************** Взаимодействия с БД ****************
+*****************************************************/
 
 const { Pool } = require('pg');
-
 const pool = new Pool({
   user: 'postgres',      
   host: 'localhost',    
@@ -9,78 +11,66 @@ const pool = new Pool({
   port: 5432,
 });
 
-
+// Получение рецептов из БД по категории
 async function getRecipes(category_id){
-  console.log("at getRecipes");
+  console.log('Вошли в обработку getRecipes');
   const client = await pool.connect();
 
   try {
-      const insertQuery = `
-        select name from cooking.recipes where category_id = $1 order by name;`;
-      //TODO попробовать в качестве категории передать ;DROP название таблицы;
-/*
-    todo: поменять позиционные параметры на именованные 
-    должно быть так 
+    
     const query = 'SELECT * FROM recipes WHERE category_id = :categoryId';
     const values = {
-      categoryId: categoryId,
-      name: name
+      categoryId: categoryId
     };
-
-*/
-
-      const values = [category_id];
-      
-      const response = await client.query(insertQuery, values);
-
-      console.log("object added to DB")
-
-      return response.rows;
+    const response = await client.query(query, values);
+    console.log('Данные получены');
+    return response.rows;
   
-    } catch (err) {
-      console.error('Ошибка при выполнении запроса:', err);
-      return [];
-    } finally {
-      // Всегда освобождаем клиента
-      client.release();
-    }
+  } catch (err) {
+    console.error('Ошибка при выполнении запроса:', err);
+    return [];
+  } finally {
+    // Всегда освобождаем клиента
+    client.release();
+  }
 
 }
 
+//Добавление рецепта в БД
 async function insertRecipe(body){
-    console.log("at insertRecipe");
-    const client = await pool.connect();
+  console.log('Вошли в обработку insertRecipe');
+  const client = await pool.connect();
 
-    try {
+  try {
     
-        const json = JSON.parse(body);
+    const json = JSON.parse(body);
+    
+    // TODO попробовать передать DROP TABLE;
+    const insertQuery = 
+      `INSERT INTO cooking.recipes (name, content, category_id)
+       VALUES ($1, $2, $3);`;
+        
+    const values = [json.title, json.recipe, json.category_id];    
+    await client.query(insertQuery, values);
+        
+    // Фиксация транзакции
+    await client.query('COMMIT');
+    console.log('Объект добавлен в БД')
+     return 200;
+    
+  } catch (err) {
+    console.error('Ошибка при выполнении запроса: ', err);
+    // Не прошла проверка на уникальность поля name
+    if(err.code == '23505'){
+        return 400;
+    }
+    await client.query('ROLLBACK');
+    return 500;
 
-        const insertQuery = `
-          INSERT INTO cooking.recipes (name, content, category_id)
-          VALUES ($1, $2, $3);`;
-        
-        const values = [json.title, json.recipe, json.category_id];
-        
-        await client.query(insertQuery, values);
-        
-        // Фиксация транзакции
-        await client.query('COMMIT');
-        console.log("object added to DB")
-        return 200;
-    
-      } catch (err) {
-    
-        if(err.code == '23505'){
-          /* Не прошла проверка на уникальность поля name */
-            return 400;
-        }
-        await client.query('ROLLBACK');
-        console.error('Ошибка при выполнении запроса:', err);
-        return 500;
-      } finally {
-        // Всегда освобождаем клиента
-        client.release();
-      }
+  } finally {
+    // Всегда освобождаем клиента
+    client.release();
+  }
 
 }
 
