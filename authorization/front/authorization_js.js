efcef0ec64a159f848f95538afbdf71ad3eb1350
Arina <arina.safronova@inbox.ru>
2025-04-06 01:08:06 +0700

    Добавлено шифрование

--- authorization/front/authorization_js.js
+++ authorization/front/authorization_js.js
@@ -1,10 +1,13 @@
 
 function signIn(){
 
+    storedPassword = '0bea2b1f68fecde452eb363247841f5a846f7a7f7fe89cf712fe9792526e7928';
     login = document.getElementById('login').value;;
     password = document.getElementById('password').value;
+    hashedPassword = sha256(password);
+    hashedPassword = sha256(hashedPassword)
 
-    if(login == '1' && password == '1' ){
+    if(login === '1' && hashedPassword === storedPassword ){
         window.location.href = '/cooking/front/cooking.html';
     } else {
         alert('Неверный логин или пароль');
